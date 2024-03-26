024eaade181e3d6a160c9e87f4fa128885752042
AtomicAnt27 <pedrodafonsecaschwertner@gmail.com>
2024-03-26 12:25:30 -0300

    kjhblkjlhjlkh

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import style from './Card.module.css'
+
+export default function Card(props){
+    return(
+        <div className={style.wrapCard}>
+            <h2>{props.title}</h2>
+            <img src={props.image}
+                 alt={props.title}
+                 width={200}
+                 height="auto"/>
+            <p>{props.text1}</p>
+        </div>
+    )
+}
